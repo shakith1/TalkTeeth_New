4ae90e4a8ef60c9beeebd329b4d7df696d0b7d18
Shakith <nimsara.shakith@gmail.com>
2020-10-09 10:47:14 +0530

    Added Add Doctor Part

--- app/src/main/java/com/example/talkteeth/add_doctor.java
+++ app/src/main/java/com/example/talkteeth/add_doctor.java
@@ -1,18 +1,33 @@
 package com.example.talkteeth;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.Calendar;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class add_doctor extends Fragment {
+
+    TextInputEditText date;
+    DatePickerDialog.OnDateSetListener dateSetListener;
 
     public add_doctor() {
         // Required empty public constructor
@@ -23,6 +38,43 @@ public class add_doctor extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_doctor, container, false);
+        View view =  inflater.inflate(R.layout.fragment_add_doctor, container, false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        date = (TextInputEditText) view.findViewById(R.id.birthday_text);
+
+        date.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                DatePickerDialog dialog = new DatePickerDialog(
+                        getContext(),
+                        android.R.style.Theme_Holo_Light_Dialog_MinWidth,
+                        dateSetListener,
+                        year,month,day);
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+                dialog.show();
+            }
+        });
+
+        dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int day) {
+                month = month + 1;
+
+                String dateSet = month + "/" + day + "/" + year;
+                date.setText(dateSet);
+            }
+        };
+
     }
 }
