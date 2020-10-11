2ca98ea1bb0549fb11e4b48995b91379a1343891
Dheeshana <harshinilalindra@gmail.com>
2020-10-11 05:48:21 +0530

    photo library added

--- app/src/main/java/com/example/talkteeth/photo_library.java
+++ app/src/main/java/com/example/talkteeth/photo_library.java
@@ -1,58 +1,41 @@
 package com.example.talkteeth;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link photo_library#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.io.File;
+
+
 public class photo_library extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    Button add, delete;
+    private static int RESULT_LOAD_IMAGE = 1;
+    ImageView image1 , image2;
+    ImageView image;
 
     public photo_library() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment photo_library.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static photo_library newInstance(String param1, String param2) {
-        photo_library fragment = new photo_library();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -60,5 +43,69 @@ public class photo_library extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_photo_library, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        add = (Button) view.findViewById(R.id.addPhoto);
+        delete = (Button) view.findViewById(R.id.delPhoto);
+
+        //img = (ImageView) view.findViewById(R.id.image_upload);
+
+        image1 = (ImageView) view.findViewById(R.id.imageView12);
+        image2 = (ImageView) view.findViewById(R.id.imageView13) ;
+
+        add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
+        image1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+              image = image1;
+
+            }
+        });
+
+        image2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                image = image2;
+            }
+        });
+
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+                builder.setCancelable(true);
+                builder.setTitle("Deleting Photo!");
+                builder.setMessage("Do You Really Want to delete this Photo!!");
+
+                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+                    }
+                });
+
+                builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        image.setImageDrawable(null);
+                    }
+                });
+                builder.show();
+
+
+            }
+        });
     }
 }
